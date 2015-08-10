f4979bf50f23542be9f9de6722c2ef414dc5e682
Adam Honoré <adam.honore@gmail.com>
2015-08-10 13:41:53 +0200

    update

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 $(function() {
 
-  $("#link_about").on('click', function() {
+  $('.nav>li>a').on('click', function() {
     event.preventDefault();
-    $("#about").show();
-    $("#link_about").find('span').show();
+
+    $('.nav>li').removeClass('active'); // Remove active button indicator
+    $('.panel-body>div').hide(); // Hide all content
+    $('.panel').show(); // Show content container
+
+    var content = $(this).attr('href'); // Get target content
+    $(content).show(); // Show target content
+    $(this).parent().addClass('active'); // Add active button indicator
   });
 
 });
